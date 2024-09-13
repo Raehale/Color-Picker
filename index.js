2136afcb4a8da4a0f0c9dc2028f68926f9b9d6d1
Raehale <alyssarae98ha@gmail.com>
2024-09-12 21:06:37 -0600

    made changes suggested by mentor

--- index.js
+++ index.js
@@ -5,31 +5,37 @@ const colorSchemeSelect = document.getElementById('colorScheme');
 const colorsEl = document.getElementById('colors');
 
 // displays colors on load
-renderColorsArr('7a3679', 'monochrome');
+fetchColors('7a3679', 'monochrome');
 
 // displays color after get color scheme button click
 getColorSchemeBtn.addEventListener('click', function(){
     const seedColorHexCode = (seedColorEl.value).substring(1);
     const colorScheme = colorSchemeSelect.value;
-    renderColorsArr(seedColorHexCode, colorScheme);
+    fetchColors(seedColorHexCode, colorScheme);
 });
 
 // copies hex code of selected color on double click
 colorsEl.addEventListener('dblclick', function(event){
     const copiedTextModal = document.getElementById('copiedTextModal');
     if (event.target.dataset.color) {
-        navigator.clipboard.writeText(event.target.dataset.color);
-        // alert(`You just coppied ${event.target.dataset.color} to your clipboard!`)
-        document.getElementById('modalText').innerHTML = `You just copied ${event.target.dataset.color} to your clipboard!`
-        copiedTextModal.style.display = 'grid';
-        setTimeout(function(){
-            copiedTextModal.style.display = 'none';
-        }, 2500);
+        navigator.clipboard.writeText(event.target.dataset.color)
+            .then(() => renderModal('Success!', `You just copied ${event.target.dataset.color} to your clipboard!`))
+            .catch(error => renderModal('Error!', error));
     }
 })
 
+// renders the modal with specified title and text
+function renderModal(title, text) {
+    document.getElementById('modalHeader').textContent = title;
+    document.getElementById('modalText').textContent = text
+    copiedTextModal.style.display = 'grid';
+    setTimeout(function(){
+        copiedTextModal.style.display = 'none';
+    }, 2500);
+}
+
 // renders an array of colors with a chosen seed color and color scheme
-function renderColorsArr(seedColorHexCode, colorScheme) {
+function fetchColors(seedColorHexCode, colorScheme) {
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColorHexCode}&mode=${colorScheme}&count=6`, {
             method: 'GET',
             headers: {
@@ -38,11 +44,11 @@ function renderColorsArr(seedColorHexCode, colorScheme) {
             }
         })
         .then(response => response.json())
-        .then(data => displayColors(data.colors))
+        .then(data => renderColors(data.colors))
 }
 
 // displays the rendered colors in the main content
-function displayColors(colorsArr) {
+function renderColors(colorsArr) {
     const displayedColors = colorsArr.map(function(color){
         return /*HTML*/ `
                 <div class="color">
